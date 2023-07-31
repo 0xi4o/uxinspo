5e0f71377d60488c6e4de9cd90dc9132dd470da1
Ilango <rajagopalilango@gmail.com>
2023-07-31 14:14:39 +0530

    📦 NEW: Add link and social links components

---
+++ src/data/presentation.ts
@@ -0,0 +1,31 @@
+type Social = {
+    label: string
+    link: string
+}
+
+type Presentation = {
+    logo: string
+    mail: string
+    title: string
+    description: string
+    socials: Social[]
+}
+
+const presentation: Presentation = {
+    logo: '',
+    mail: '',
+    title: '',
+    description: '',
+    socials: [
+        {
+            label: 'Twitter',
+            link: 'https://twitter.com/i4o_dev',
+        },
+        {
+            label: 'Github',
+            link: 'https://github.com/i4o-dev/saas-ui-inspo',
+        },
+    ],
+}
+
+export default presentation
