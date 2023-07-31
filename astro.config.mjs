5297361a56249b0127152b3b05ccfce2ddd28ad3
Ilango <rajagopalilango@gmail.com>
2023-07-31 20:29:32 +0530

    👌 IMPROVE: Add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import compress from "astro-compress";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
+import compress from 'astro-compress'
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
+
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://unwrapped.design',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
-});
+    site: 'https://ui-inspo.vercel.app/',
+    integrations: [
+        tailwind(),
+        image({
+            serviceEntryPoint: '@astrojs/image/sharp',
+        }),
+        compress(),
+        partytown(),
+        sitemap(),
+        mdx(),
+    ],
+})
