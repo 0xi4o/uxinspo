9eb62d2cfad823c45ffd869cfbcdb3d4b332818e
Ilango <rajagopalilango@gmail.com>
2023-07-31 20:30:59 +0530

    👌 IMPROVE: Apply prettier

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,31 +1,31 @@
 type Social = {
-    label: string
-    link: string
+	label: string
+	link: string
 }
 
 type Presentation = {
-    logo: string
-    mail: string
-    title: string
-    description: string
-    socials: Social[]
+	logo: string
+	mail: string
+	title: string
+	description: string
+	socials: Social[]
 }
 
 const presentation: Presentation = {
-    logo: '',
-    mail: '',
-    title: '',
-    description: '',
-    socials: [
-        {
-            label: 'Twitter',
-            link: 'https://twitter.com/i4o_dev',
-        },
-        {
-            label: 'Github',
-            link: 'https://github.com/i4o-dev/saas-ui-inspo',
-        },
-    ],
+	logo: '',
+	mail: '',
+	title: '',
+	description: '',
+	socials: [
+		{
+			label: 'Twitter',
+			link: 'https://twitter.com/i4o_dev',
+		},
+		{
+			label: 'Github',
+			link: 'https://github.com/i4o-dev/saas-ui-inspo',
+		},
+	],
 }
 
 export default presentation
