482e2b93f3530332161051ac2bf1d155f3458a3b
Ilango <rajagopalilango@gmail.com>
2023-08-01 19:20:15 +0530

    👌 IMPROVE: Apply prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,18 @@ import { UiCategories } from '@/utils/types'
 import { defineCollection, z } from 'astro:content'
 
 const uiCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        product: z.string(),
-        excerpt: z.string().default(''),
-        image: z.string(),
-        category: z.enum(UiCategories),
-        website: z.string().default(''),
-        date_published: z.date(),
-        draft: z.boolean().default(false),
-    }),
+	type: 'content',
+	schema: z.object({
+		product: z.string(),
+		excerpt: z.string().default(''),
+		image: z.string(),
+		category: z.enum(UiCategories),
+		website: z.string().default(''),
+		date_published: z.date(),
+		draft: z.boolean().default(false),
+	}),
 })
 
 export const collections = {
-    ui: uiCollection,
+	ui: uiCollection,
 }
