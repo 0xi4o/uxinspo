7763792b0a047c7028a908c975c0ba1b602b253b
Ilango <rajagopalilango@gmail.com>
2023-08-02 14:14:26 +0530

    👌 IMPROVE: Rename dashboard to dashboards everywhere

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,29 +2,29 @@ import { UiCategories } from '@/utils/types'
 import { z } from 'astro:content'
 
 export const CategoryItem = z.object({
-	image: z.string(),
-	title: z.string(),
-	type: z.enum(UiCategories),
+    image: z.string(),
+    title: z.string(),
+    type: z.enum(UiCategories),
 })
 
 export type CategoryItem = z.infer<typeof CategoryItem>
 
 const categories: CategoryItem[] = [
-	{
-		image: '/thumbnails/dashboard.png',
-		title: 'Dashboard',
-		type: 'dashboard',
-	},
-	{
-		image: '/thumbnails/settings.png',
-		title: 'Settings',
-		type: 'settings',
-	},
-	{
-		image: '/thumbnails/lists-tables.png',
-		title: 'Lists & Tables',
-		type: 'lists-tables',
-	},
+    {
+        image: '/thumbnails/dashboards.png',
+        title: 'Dashboards',
+        type: 'dashboards',
+    },
+    {
+        image: '/thumbnails/settings.png',
+        title: 'Settings',
+        type: 'settings',
+    },
+    {
+        image: '/thumbnails/lists-tables.png',
+        title: 'Lists & Tables',
+        type: 'lists-tables',
+    },
 ]
 
 export default categories
