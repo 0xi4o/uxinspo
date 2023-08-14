2eaf63b8eaa70de0d31317e6bb12206ace19368f
Ilango <rajagopalilango@gmail.com>
2023-08-14 14:16:56 +0530

    👌 IMPROVE: Apply prettier everywhere

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,20 +2,20 @@ import { UiCategories } from '@/utils/types'
 import { defineCollection, z } from 'astro:content'
 
 const uiCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        product: z.string(),
-        excerpt: z.string().default(''),
-        image: z.union([z.string(), z.string().array()]),
-        thumbnail: z.string().default(''),
-        group: z.boolean().default(false),
-        category: z.enum(UiCategories),
-        website: z.string().default(''),
-        date_published: z.date(),
-        draft: z.boolean().default(false),
-    }),
+	type: 'content',
+	schema: z.object({
+		product: z.string(),
+		excerpt: z.string().default(''),
+		image: z.union([z.string(), z.string().array()]),
+		thumbnail: z.string().default(''),
+		group: z.boolean().default(false),
+		category: z.enum(UiCategories),
+		website: z.string().default(''),
+		date_published: z.date(),
+		draft: z.boolean().default(false),
+	}),
 })
 
 export const collections = {
-    ui: uiCollection,
+	ui: uiCollection,
 }
