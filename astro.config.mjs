06cd637133da5dce89ee10c40b02f0d5bfdaa5ef
Ilango <rajagopalilango@gmail.com>
2023-11-15 12:35:23 +0530

    👌 IMPROVE: Replace astrojs/image with astro assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, squooshImageService } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
 import compress from 'astro-compress'
 import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
@@ -8,15 +7,9 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+	image: {
+		service: squooshImageService(),
+	},
+	integrations: [tailwind(), compress(), partytown(), sitemap(), mdx()],
 	site: 'https://uxinspo.vercel.app/',
-	integrations: [
-		tailwind(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
-		compress(),
-		partytown(),
-		sitemap(),
-		mdx(),
-	],
 })
